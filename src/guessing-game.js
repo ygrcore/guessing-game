3fefbb605229987bb6b5876e1b882326aaf9b3fa
Egor <itpr2010@yandex.ru>
2023-02-09 11:51:20 +0300

    feat: binary search algorithm

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,18 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.midValue = Math.round(((this.maxValue - this.minValue)/ 2) + this.minValue);
+        return this.midValue;
     }
 
     lower() {
-
+        this.setRange(this.minValue, this.midValue)
     }
 
     greater() {
+        this.setRange(this.midValue, this.maxValue)
 
     }
 }
